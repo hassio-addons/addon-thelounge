f4500ad10d1cb7407268fe91b3cfb24e45fa6296
Timmo <contact@timmo.xyz>
2019-02-09 22:10:01 +0000

    :sparkles: Add add-on code and setup

---
+++ thelounge/rootfs/etc/cont-init.d/20-setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: The Lounge
+# This adds the default user and installs any requested themes
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+export THELOUNGE_HOME=/data/thelounge
+
+if ! hass.directory_exists "/data/thelounge"; then
+    hass.log.info "Creating default hassio user.."
+    mkdir -p /data/thelounge/users
+    cp /etc/thelounge/users/hassio.json /data/thelounge/users
+else
+    for theme in $(hass.config.get "themes")
+    do
+        /usr/local/bin/thelounge install "$theme"
+    done
+fi
