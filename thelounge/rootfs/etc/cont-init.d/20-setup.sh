1e23014cbd56df09a9a34bcd68cfd69af4d4f100
Timmo <contact@timmo.xyz>
2019-03-19 22:01:23 +0000

    :arrow_up: Upgrade to base 3.0.0 and migrate to bashio

--- thelounge/rootfs/etc/cont-init.d/20-setup.sh
+++ thelounge/rootfs/etc/cont-init.d/20-setup.sh
@@ -1,19 +1,16 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: The Lounge
 # This adds the default user and installs any requested themes
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 export THELOUNGE_HOME=/data/thelounge
 
-if ! hass.directory_exists "/data/thelounge"; then
-    hass.log.info "Creating default hassio user.."
+if ! bashio::fs.directory_exists "/data/thelounge"; then
+    bashio::log.info "Creating default hassio user.."
     mkdir -p /data/thelounge/users
     cp /etc/thelounge/users/hassio.json /data/thelounge/users
 else
-    for theme in $(hass.config.get "themes")
+    for theme in $(bashio::config "themes")
     do
         /usr/local/bin/thelounge install "$theme"
     done
