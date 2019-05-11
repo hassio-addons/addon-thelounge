ea58482befdad31967eb2f0b59a5a8ef386df5c4
Timmo <contact@timmo.xyz>
2019-05-11 02:42:37 +0100

    :sparkles: Allow multiple users
    Closes #5

--- thelounge/rootfs/etc/cont-init.d/thelounge.sh
+++ thelounge/rootfs/etc/cont-init.d/thelounge.sh
@@ -6,8 +6,14 @@
 export THELOUNGE_HOME=/data/thelounge
 
 if ! bashio::fs.directory_exists "/data/thelounge"; then
+    mkdir -p  /data/thelounge
+fi
+
+if ! bashio::fs.directory_exists "/config/thelounge/users"; then
+    bashio::log.info "Creating thelounge directory in /config.."
+    mkdir -p /config/thelounge/users
+    ln -sf /config/thelounge/users /data/thelounge/users
     bashio::log.info "Creating default hassio user.."
-    mkdir -p /data/thelounge/users
     cp /etc/thelounge/users/hassio.json /data/thelounge/users
 else
     for theme in $(bashio::config "themes")
