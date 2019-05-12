2dae14468f6143fc2ba3fccce7d7e0a73126818a
Timmo <contact@timmo.xyz>
2019-05-12 14:31:05 +0100

    Revert ":hammer: Switch config directory to /config/thelounge"
    This reverts commit 6d4e98cbaa0fddb96c1e22cb9b952484f50c63bb.

--- thelounge/rootfs/etc/cont-init.d/thelounge.sh
+++ thelounge/rootfs/etc/cont-init.d/thelounge.sh
@@ -3,11 +3,16 @@
 # Community Hass.io Add-ons: The Lounge
 # This adds the default user and installs any requested themes
 # ==============================================================================
-export THELOUNGE_HOME=/config/thelounge
+export THELOUNGE_HOME=/data/thelounge
+
+if ! bashio::fs.directory_exists "/data/thelounge"; then
+    mkdir -p  /data/thelounge
+fi
 
 if ! bashio::fs.directory_exists "/config/thelounge/users"; then
     bashio::log.info "Creating thelounge directory in /config.."
     mkdir -p /config/thelounge/users
+    ln -sf /config/thelounge/users /data/thelounge/users
     bashio::log.info "Creating default hassio user.."
     cp /etc/thelounge/users/hassio.json /config/thelounge/users
 else
