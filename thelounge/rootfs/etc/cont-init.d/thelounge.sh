88c0f8c4ad3c1ec572ce6c51e3c5a8b7429018d5
Timmo <contact@timmo.xyz>
2019-05-12 14:33:17 +0100

    Revert ":sparkles: Allow multiple users"
    This reverts commit ea58482befdad31967eb2f0b59a5a8ef386df5c4.

--- thelounge/rootfs/etc/cont-init.d/thelounge.sh
+++ thelounge/rootfs/etc/cont-init.d/thelounge.sh
@@ -6,15 +6,9 @@
 export THELOUNGE_HOME=/data/thelounge
 
 if ! bashio::fs.directory_exists "/data/thelounge"; then
-    mkdir -p  /data/thelounge
-fi
-
-if ! bashio::fs.directory_exists "/config/thelounge/users"; then
-    bashio::log.info "Creating thelounge directory in /config.."
-    mkdir -p /config/thelounge/users
-    ln -sf /config/thelounge/users /data/thelounge/users
     bashio::log.info "Creating default hassio user.."
-    cp /etc/thelounge/users/hassio.json /config/thelounge/users
+    mkdir -p /data/thelounge/users
+    cp /etc/thelounge/users/hassio.json /data/thelounge/users
 else
     for theme in $(bashio::config "themes")
     do
