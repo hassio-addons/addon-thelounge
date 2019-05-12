db7a338de8ecd087e19cf0a02bd610ce45ee535a
Timmo <contact@timmo.xyz>
2019-05-12 16:30:33 +0100

    :sparkles: Support multiple users

--- thelounge/rootfs/etc/cont-init.d/thelounge.sh
+++ thelounge/rootfs/etc/cont-init.d/thelounge.sh
@@ -6,12 +6,21 @@
 export THELOUNGE_HOME=/data/thelounge
 
 if ! bashio::fs.directory_exists "/data/thelounge"; then
-    bashio::log.info "Creating default hassio user.."
     mkdir -p /data/thelounge/users
-    cp /etc/thelounge/users/hassio.json /data/thelounge/users
-else
-    for theme in $(bashio::config "themes")
-    do
-        /usr/local/bin/thelounge install "${theme}"
-    done
 fi
+
+# List current users
+/usr/local/bin/thelounge list
+
+for user in $(bashio::config "users")
+do
+    if ! bashio::fs.file "/data/thelounge/users/${user}.json"; then
+        bashio::log.info "Creating user ${user} with default password.."
+        cp /etc/thelounge/users/hassio.json "/data/thelounge/users/${user}.json"
+    fi
+done
+
+for theme in $(bashio::config "themes")
+do
+    /usr/local/bin/thelounge install "${theme}"
+done
